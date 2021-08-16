f5d47fd9427fac67f995ea45f991e93ce382e22e
zhemchugov_pe <zhemchugov.p@gmail.com>
2021-08-16 23:59:25 +0300

    add bild

---
+++ docs/js/js.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var $page1 = document.getElementsByClassName('page--1')[0];
+window.addEventListener('scroll', function () {
+  var offsetPercent = Math.floor(this.scrollY * 100 / this.innerHeight);
+  $page1.style.width = "".concat(offsetPercent, "%");
+});
