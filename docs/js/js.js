f9c0ec14710a957b09265edba4333bea68e672f3
zhemchugov_pe <zhemchugov.p@gmail.com>
2021-08-24 22:55:51 +0300

    ain

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,7 +1,38 @@
 "use strict";
 
-var $page1 = document.getElementsByClassName('page--1')[0];
+var $page1 = $('.page--1');
+var $text1 = $('.text--1');
+var $text2 = $('.text--2');
+var $text3 = $('.text--3');
+var $text4 = $('.text--4');
+var $text5 = $('.text--5');
+var $part1 = $('.part--1');
+var $part2 = $('.part--2');
+var $part3 = $('.part--3');
+var $part4 = $('.part--4');
 window.addEventListener('scroll', function () {
-  var offsetPercent = Math.floor(this.scrollY * 100 / this.innerHeight);
-  $page1.style.width = "".concat(100 - offsetPercent / 2, "%");
+  var offsetPercent = Math.floor(this.scrollY * 100 / this.innerHeight) / 2;
+  var percentOfAnimation = offsetPercent > 100 ? 100 : offsetPercent;
+  setBackgroundPosition(percentOfAnimation);
+  moveBlock($text1, 10, percentOfAnimation);
+  moveBlock($text2, -20, percentOfAnimation);
+  moveBlock($text3, 5, percentOfAnimation);
+  moveBlock($text4, -5, percentOfAnimation);
+  moveBlock($text5, -50, percentOfAnimation);
+  moveBlock($part1, -15, percentOfAnimation);
+  moveBlock($part2, -35, percentOfAnimation);
+  moveBlock($part3, -10, percentOfAnimation);
+  moveBlock($part4, -20, percentOfAnimation);
 });
+
+var setBackgroundPosition = function setBackgroundPosition(percentOfAnimation) {
+  $page1.css({
+    width: "".concat(100 - percentOfAnimation, "%")
+  });
+};
+
+var moveBlock = function moveBlock($textBlock, distance, percentOfAnimation) {
+  $textBlock.css({
+    transform: "translatex(".concat(distance * percentOfAnimation / 100, "vw)")
+  });
+};
--- src/js/js.js
+++ src/js/js.js
@@ -1,5 +1,42 @@
-let $page1 = document.getElementsByClassName('page--1')[0];
-window.addEventListener('scroll', function (){
-    const offsetPercent = Math.floor(this.scrollY * 100 / this.innerHeight)
-    $page1.style.width = `${100 - offsetPercent/2}%`;
-})
+const $page1 = $('.page--1');
+
+const $text1 = $('.text--1');
+const $text2 = $('.text--2');
+const $text3 = $('.text--3');
+const $text4 = $('.text--4');
+const $text5 = $('.text--5');
+
+const $part1 = $('.part--1');
+const $part2 = $('.part--2');
+const $part3 = $('.part--3');
+const $part4 = $('.part--4');
+
+window.addEventListener('scroll', function () {
+    const offsetPercent = Math.floor(this.scrollY * 100 / this.innerHeight) / 2;
+    const percentOfAnimation = offsetPercent > 100 ? 100 : offsetPercent;
+
+    setBackgroundPosition(percentOfAnimation);
+    moveBlock($text1, 10, percentOfAnimation);
+    moveBlock($text2, -20, percentOfAnimation);
+    moveBlock($text3, 5, percentOfAnimation);
+    moveBlock($text4, -5, percentOfAnimation);
+    moveBlock($text5, -50, percentOfAnimation);
+
+    moveBlock($part1, -15, percentOfAnimation);
+    moveBlock($part2, -35, percentOfAnimation);
+    moveBlock($part3, -10, percentOfAnimation);
+    moveBlock($part4, -20, percentOfAnimation);
+});
+
+const setBackgroundPosition = (percentOfAnimation) => {
+    $page1.css({
+        width: `${100 - percentOfAnimation}%`
+    });
+}
+
+const moveBlock = ($textBlock, distance, percentOfAnimation) => {
+    $textBlock.css ({
+        transform: `translatex(${distance * percentOfAnimation / 100}vw)`
+    });
+}
+
